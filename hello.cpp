fd0e241089e340ec24cb377dfaa3a9c59ab9f40d
NIKH10001 <nikhilbhagat0005@gmail.com>
2024-09-28 05:41:44 -0400

    First program
    hello world

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+//using type casting explict conversion 
+#include<iostream>
+using namespace std;
+int main()
+{
+  float pi =3.14;
+  cout << (int)(pi)<< endl; 
+  cout << ((float)(10/3))<<endl;
+  cout << ((char)('A'+1)) <<endl;
+  return 0; 
+}
